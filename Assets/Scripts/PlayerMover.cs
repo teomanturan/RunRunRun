d06996b8a48038d3f544c55b79a79c8d9c22dd8a
teoser99 <57498915+teomanturan@users.noreply.github.com>
2023-05-10 00:41:59 +0300

    Character Horizontal Movement

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class PlayerMover : MonoBehaviour
 {
     public float speed = 10;
+    public float horizontalSpeed = 30;
     float horizontalInput;
+    int lane = 1;
+    float laneDistance = 6;
 
     private void FixedUpdate()
     {
@@ -25,12 +28,36 @@ public class PlayerMover : MonoBehaviour
     #endregion
 
     #region Yatay Hareket
-
     void HorizontalMover()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
-    }
+        if(Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            lane++;
+            if(lane == 3)
+            {
+                lane = 2;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            lane--;
+            if(lane == -1)
+            {
+                lane = 0;
+            }
+        }
+        Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
 
+        if (lane == 0)
+        {
+            targetPosition += Vector3.left * laneDistance;
+        }
+        else if (lane == 2)
+        {
+            targetPosition += Vector3.right * laneDistance;
+        }
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, 3 * Time.fixedDeltaTime);
+    }
     #endregion
 }//Class
